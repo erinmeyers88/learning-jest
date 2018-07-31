4bd7465f4e23a89877b8afa635770e09ddac463e
Vincent, Erin <evincent@ariit.com>
2018-07-31 11:23:29 -0600

    before and after globals

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,27 @@
 describe("The Question List", () => {
+
+  beforeEach(() => {
+    console.log('Before each!');
+  });
+
+  beforeAll(() => {
+    console.log('Before all!');
+  });
+
+  afterEach(() => {
+    console.log('After each!');
+  });
+
+  afterAll(() => {
+    console.log('After all!');
+  });
+
   it("should display a list of items", () => {
     expect(40 + 2).toEqual(42);
   });
+
+  it("should be the meaning of life", () => {
+    expect(40 + 2).toEqual(42);
+  });
+
 });
